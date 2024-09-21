beef6506278cac850412757f599e581a7b2dee8b
maryaposohova <maryaposohova1202@gmail.com>
2024-09-21 22:59:14 +0500

    Add files via upload

---
+++ module_4/func_sort.py
@@ -0,0 +1,39 @@
+
+# Сортировка пузырьком
+def bubble_sort(ls):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(
+                len(ls) - 1):
+            if ls[i] > ls[i + 1]:
+                ls[i], ls[i + 1] = ls[i + 1], ls[i]
+                swapped = True
+    return ls
+
+# Сортировка выборкой
+def selection_sort(ls):
+    for i in range(
+            len(ls)):
+        lowest = i
+        for j in range(i + 1, len(ls)):
+            if ls[j] < ls[lowest]:
+                lowest = j
+        ls[i], ls[lowest] = ls[lowest], ls[i]
+    return ls
+
+
+
+
+# Сортировка вставкой
+def insertion_soft(ls):
+    for i in range(1, len(ls)):
+        key = ls[i]
+        j = i - 1
+        while ls[j] > key and j >= 0:
+            ls[j + 1] = ls[
+                j]
+            j -= 1
+        ls[j + 1] = key
+    return ls
+
